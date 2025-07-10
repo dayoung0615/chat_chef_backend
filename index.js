586ac1607b6cd9d60f6b3ccacf8ae9dd18c118b5
dayoung <vkvidizl@gmail.com>
2025-07-10 10:45:50 +0900

    env

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-console.log("안녕");
 //npm init -y
 //yarn add express cors openai dotenv
+
+import express from "express";
+import cors from "cors";
+import * as dotanv from "dotenv";
+//   "type": "module", < 최신방식(package.json)
+
+const app = express();
+// const cors = cors();
+
+app.use(cors());
+const __dirname = path.resolve();
+dotanv.config({
+  path: __dirname + "/.env",
+});
+const port = process.env.PORT;
+
+app.listen("8080", () => {
+  //port번호 화인.
+  console.log("port:", port);
+});
